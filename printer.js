15b1fff024949734f60091f813831b606efcc234
shugo9538 <shugo9538@gmail.com>
2020-07-01 23:35:57 +0900

    Update printer.js

--- printer.js
+++ printer.js
@@ -1,5 +1,10 @@
+/*
+ * how to print in javascript
+*/
+
 console.log("hello %s", "universe");
 console.log("Score : %d", 10);
 
+// cf) not '', it's ``
 let myMsg = "universe";
 console.log(`Hello, ${myMsg}`);
