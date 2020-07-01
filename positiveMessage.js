922f6b2931fdfcb694b6339169bfbb7e4a05f147
shugo9538 <shugo9538@gmail.com>
2020-07-01 23:29:48 +0900

    Add files via upload

---
+++ positiveMessage.js
@@ -0,0 +1,8 @@
+let messages = [
+  "A change of environment can be a good thing!",
+    "You will make it!",
+      "Just run with the code!"
+      ];
+      
+"use strict";
+messages.forEach(message => console.log(message));
---
+++ printer.js
@@ -0,0 +1,5 @@
+console.log("hello %s", "universe");
+console.log("Score : %d", 10);
+
+let myMsg = "universe";
+console.log(`Hello, ${myMsg}`);
