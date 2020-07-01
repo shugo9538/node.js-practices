f6cd0688e7906f8e18ffd3dfccf457c86abb6526
shugo9538 <shugo9538@gmail.com>
2020-07-01 23:34:04 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,3 +1,8 @@
+/*
+ * This javascript is used for positiveMessage.js
+ * This is changed in commandline
+*/
+
 let messages = [
   "A change of environment can be a good thing!",
   "You will make it!",
